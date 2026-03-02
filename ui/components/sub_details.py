ac86f523ffd48523557c4bd2b350f4f33e9e51bf
noelarca <dumenoe018@gmail.com>
2026-03-02 11:30:05 +0100

    Restore of working code

--- ui/components/sub_details.py
+++ ui/components/sub_details.py
@@ -1,7 +1,6 @@
 from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QLabel, QListWidget
 )
-from PySide6.QtGui import QFont
 
 class SubDetails(QWidget):
     def __init__(self, viewmodel=None):
@@ -9,13 +8,17 @@ class SubDetails(QWidget):
         self.viewmodel = viewmodel
 
         self.layout = QVBoxLayout(self)
-        self.details_list = QVBoxLayout()
+
+        self.title = QLabel("Dettagli materia:")
+        self.details_list = QListWidget()
 
         self.layout.addWidget(self.title)
+        self.layout.addWidget(self.details_list)
 
     def load_details(self, subject):
-        self.title = QLabel()
-        self.title.setFont(QFont("Arial", 14, QFont.Bold))
         self.details_list.clear()  # Pulisce la lista prima di ricaricare
         details = self.viewmodel.get_subject_details(subject)
-        self.title.setText(f"Dettagli per: {details.get('name', 'N/A')}")
+        if details:
+            for key, value in details.items():
+                label = "CFU" if key == "credits" else key.capitalize()
+                self.details_list.addItem(f"{label}: {value}")
